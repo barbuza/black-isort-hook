aa677d25306071d61c1981eda485d120b434e326
Victor Kotseruba <barbuzaster@gmail.com>
2023-10-10 08:34:00 -0700

    confirm before removing existing hook (#4)

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,20 +1,37 @@
-import os
 import pathlib
 import subprocess
 
+import pytest
 
-def test_install(tmp_path: pathlib.Path) -> None:
-    """Test that the install script installs the hook correctly."""
-    start_dir = os.getcwd()
-    try:
-        os.chdir(tmp_path)
-        subprocess.run(["git", "init"], check=True, capture_output=True)
-        hook_path = tmp_path / ".git" / "hooks" / "pre-commit"
-        assert not hook_path.exists()
-        subprocess.run(
-            ["install-black-isort-hook"], check=True, capture_output=True
-        )
-        assert hook_path.exists()
-        subprocess.run([hook_path], check=True, capture_output=True)
-    finally:
-        os.chdir(start_dir)
+
+def test_install(  # noqa: D103
+    tmp_path: pathlib.Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.chdir(tmp_path)
+    subprocess.check_call(["git", "init"])
+    hook_path = tmp_path / ".git" / "hooks" / "pre-commit"
+    assert not hook_path.exists() and not hook_path.is_symlink()
+    subprocess.run(
+        "install-black-isort-hook", check=True, stdin=subprocess.DEVNULL
+    )
+    assert hook_path.is_symlink()
+    assert (
+        "are you sure?"
+        in subprocess.run(
+            "install-black-isort-hook",
+            capture_output=True,
+            text=True,
+            stdin=subprocess.DEVNULL,
+        ).stderr
+    )
+    hook_path.unlink()
+    assert (
+        "` to `"
+        in subprocess.run(
+            "install-black-isort-hook",
+            capture_output=True,
+            text=True,
+            stdin=subprocess.DEVNULL,
+        ).stderr
+    )
+    subprocess.check_call(hook_path)
